39a88b6d9fd8c7e4d181611b0abd318e016ea08a
MeCode4Food <cheekhuin.loong@gmail.com>
2017-06-08 23:24:00 +0800

    First commit

---
+++ hungmen/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace hungmen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GuessBox = new System.Windows.Forms.TextBox();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.OutputBox.Location = new System.Drawing.Point(42, 12);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.Size = new System.Drawing.Size(206, 207);
+            this.OutputBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type your guess!";
+            // 
+            // GuessBox
+            // 
+            this.GuessBox.Location = new System.Drawing.Point(42, 245);
+            this.GuessBox.MaxLength = 1;
+            this.GuessBox.Name = "GuessBox";
+            this.GuessBox.Size = new System.Drawing.Size(206, 26);
+            this.GuessBox.TabIndex = 2;
+            this.GuessBox.TextChanged += new System.EventHandler(this.GuessBox_TextChanged);
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Location = new System.Drawing.Point(164, 277);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(84, 41);
+            this.GuessButton.TabIndex = 3;
+            this.GuessButton.Text = "Guess!";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.GuessButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 320);
+            this.Controls.Add(this.GuessButton);
+            this.Controls.Add(this.GuessBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox GuessBox;
+        private System.Windows.Forms.Button GuessButton;
+    }
+}
+
---
+++ hungmen/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hungmen
+{
+    public partial class Form1 : Form
+    {
+        int LivesLeft;
+        int score = 0;
+        string Guess;
+        string GuessCopy;
+        List<char> GuessedChar = new List<char>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //form1.AcceptButton = GuessButton;
+            Start();
+        }
+
+        public void Start()
+        {
+            //Word Bank initialisation
+            string[] wordBank = { "Apple", "Box", "Creation", "Dystopia", "Establishmentarian", "Flogging", "Gargantuan", "Hysteria", "Incendiary" };
+            GuessedChar.Clear();
+
+            //Game Parameters
+            LivesLeft = 7;
+
+            //Random pick from Word Bank
+            Random rand = new Random();
+            Guess = wordBank[rand.Next(0, wordBank.Length)];
+            Guess = Guess.ToUpper();
+            GuessCopy = Guess;
+
+            //MessageBox.Show(Guess);
+            //Generate OutputBox State
+            HungState(LivesLeft);
+
+            //Reset Answer State
+            AnswerState(GuessCopy);
+
+
+            //Focus Cursor
+            GuessBox.Select();
+        }
+
+        public void HungState(int LivesLeft)
+        {
+          #region 
+          switch(LivesLeft)
+            {
+                case 0:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                O    \r\n |               /|\\  \r\n |               / \\  \r\n |              \r\n";
+                    break;
+                case 1:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                O    \r\n |               /|\\  \r\n |               /    \r\n |              \r\n";
+                    break;
+                case 2:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                O    \r\n |               /|\\  \r\n |                    \r\n |              \r\n";
+                    break;
+                case 3:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                O    \r\n |               /|   \r\n |                    \r\n |              \r\n";
+                    break;
+                case 4:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                O    \r\n |                |   \r\n |                    \r\n |              \r\n";
+                    break;
+                case 5:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                O    \r\n |                    \r\n |                    \r\n |              \r\n";
+                    break;
+                case 6:
+                    OutputBox.Text = " _________     \r\n |                 |    \r\n |                     \r\n |                    \r\n |                    \r\n |              \r\n";
+                    break;
+                case 7:
+                    OutputBox.Text = " _________     \r\n |                      \r\n |                     \r\n |                    \r\n |                    \r\n |              \r\n";
+                    break;
+
+            }
+            #endregion
+        }
+            
+        public void UpdatedGuessed(List<char> List)
+        {
+            for (int i = 0; i < List.Count ; i++)
+            {
+                OutputBox.AppendText(Char.ToString(List[i]));
+                
+                if (!(i == List.Count - 1))
+                    OutputBox.AppendText(" ");
+            }
+        }
+
+        //State of answer, if alphabet,
+        private void AnswerState(string ACopy)
+        {
+            bool game = true;
+
+            for (int i = 0; i < Guess.Length; i++)
+            {
+                if (char.IsUpper(ACopy[i]))
+                {
+                    game = false;
+                    OutputBox.AppendText("_");
+                }
+                else if(char.IsLower(ACopy[i]))
+                {
+                    OutputBox.AppendText(Char.ToString(Char.ToUpper(ACopy[i])));
+                }
+                if (!(i == Guess.Length - 1))
+                    OutputBox.AppendText(" ");
+                else
+                    OutputBox.AppendText("\n");
+            }
+            if(game)
+            {
+                score++;
+                MessageBox.Show("You so good dood. Your score is now " + score + ". Restarting gaem");
+                Start();
+            }
+            
+            if(LivesLeft == 0)
+            {
+                MessageBox.Show("Game Over LUL. You so bayed. Answer is " + GuessCopy.ToUpper() +", and your score so far is " + score +".\nRestarting gaem");
+                Start();
+            }
+        }
+
+        private bool IsValid(string str)
+        {
+            return Regex.IsMatch(str, @"^[a-zA-Z]$");
+        }
+
+        private void GuessButton_Click(object sender, EventArgs e)
+        {
+            //IsValid() Test
+            //MessageBox.Show(Convert.ToString((IsValid(GuessBox.Text)))); 
+
+            //HungState() Test
+            //HungState(Convert.ToInt32(GuessBox.Text));
+
+            if (!(IsValid(GuessBox.Text)))
+            {
+                MessageBox.Show("Please enter a letter.");
+            }
+            else if (LivesLeft > 0)
+            {
+                GuessBox.Text = GuessBox.Text.ToUpper();
+
+                if (GuessedChar.Contains(Convert.ToChar(GuessBox.Text)))
+                {
+                    MessageBox.Show("Pls no repeat urself");
+                }
+
+                if(Regex.IsMatch(Guess,@"["+GuessBox.Text+@"]+"))
+                {
+                    MessageBox.Show("You so good");
+                    GuessCopy = GuessCopy.Replace(GuessBox.Text, GuessBox.Text.ToLower());
+                    GuessedChar.Add(Convert.ToChar(GuessBox.Text));
+                }
+                else
+                {
+                    MessageBox.Show("You so bad");
+                    LivesLeft--;
+                    GuessedChar.Add(Convert.ToChar(GuessBox.Text));
+                }
+
+                HungState(LivesLeft);
+                AnswerState(GuessCopy);
+                UpdatedGuessed(GuessedChar);
+            }
+        }
+
+        private void GuessBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
